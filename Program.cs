7bc69fe09f2e3bf37c62c9fd909d27d569d31f66
Chanan1 <chanan69@students.amikom.ac.id>
2023-03-21 20:02:27 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "chanan artamma";
+            taxi.Onduty = true;
+            taxi.numPassenger = 22114957;
+
+            taxi.info();
+            taxi.jemput();
+            taxi.turun();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public Boolean Onduty { get; set; }
+        public int numPassenger { get; set; }
+
+        public void info()
+        {
+            Console.WriteLine("\n Info Taxi\n");
+            Console.WriteLine("Nama Driver : {0}", DriverName);
+
+            if (Onduty == true)
+            {
+                Console.WriteLine("Narik : iya", Onduty);
+            }
+            else
+            {
+                Console.WriteLine("Narik : Tidak", Onduty);
+            }
+
+            Console.WriteLine("Nomor pelanggan : {0}", numPassenger);
+        }
+        public void jemput()
+        {
+            Console.WriteLine("\n {0} Sedang menjemput penumpang", DriverName);
+        }
+        public void turun()
+        {
+            Console.WriteLine("\n {0} Selesai mengantar penumpang", DriverName);
+
+        }
+    }
+}
